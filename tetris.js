a4f93dcd08dcb276a8e466200437f323348421b7
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-10-09 22:27:27 +1300

    rotating

--- tetris.js
+++ tetris.js
@@ -11,6 +11,17 @@ const matrix = [
     [0,1,0]
 ]
 
+function collide(arena, player){
+    const [m, o] = [player.matrix, player.pos]
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x){
+            if(m[y][x] !==0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+                return true
+            }
+        } 
+    }
+    return false;
+}
 
 function createMatrix(w,h){
     const matrix = [];
@@ -22,6 +33,7 @@ function createMatrix(w,h){
 function draw () {
     context.fillStyle = '#000';
     context.fillRect(0,0, canvas.width, canvas.height);
+    drawMatrix(arena, {x: 0, y: 0})
     drawMatrix(player.matrix, player.pos);
 }
 
@@ -51,7 +63,43 @@ function merge(arena, player) {
 
 function playerDrop() {
     player.pos.y++;
+    if(collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
     dropCounter =0;
+}
+
+function playerMove(dir) {
+    player.pos.x += dir;
+    if(collide(arena, player)){
+        player.pos.x -= dir;
+    }
+}
+
+function playerRoate(dir){
+    rotate(player.matrix, dir);
+}
+
+function rotate(matrix, dir) {
+    for (let y =0; y < matrix.length; ++y){
+        for (let x =0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ]
+        }
+    }
+    
+    if(dir > 0) {
+        matrix.forEach(row => row.reverse())
+    } else {
+        matrix.reverse();
+    }
 }
 
 let dropCounter = 0;
@@ -79,11 +127,13 @@ const player = {
 
 document.addEventListener('keydown', event => {
     if(event.keyCode === 37) {
-        player.pos.x--;
+        playerMove(-1)
     } else if (event.keyCode === 39) {
-        player.pos.x++;
+       playerMove(1)
     } else if (event.keyCode === 40){
         playerDrop()
+    } else if (event.keyCode === 81) {
+        playerRoate(1);
     }
 })
 update()
